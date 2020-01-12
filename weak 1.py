1f8c972435d02f291aefde03f689a8368bfe19e4
AKSHAY MAGOTRA <59798327+akshay89magotra@users.noreply.github.com>
2020-01-13 00:39:34 +0530

    Add files via upload

---
+++ weak 1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Coursera Capstone
+
+# #### This notebook will be mainly used for the capstone project for IBM Applied Data Science Capstone course on Coursera.
+
+# In[1]:
+
+
+#Import the pandas library as pd
+import pandas as pd
+
+
+# In[2]:
+
+
+#Import the Numpy library as np
+import numpy as np
+
+
+# In[3]:
+
+
+# Print the following the statement: Hello Capstone Project Course!
+print("Hello Caps# 2. Import the pandas library as pd
+import pandas as pdtone Project Course!")
+
+
+# In[ ]:
+
+
+
+
